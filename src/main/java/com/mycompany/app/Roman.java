050dae61bbab1be6ae59db0d67f3ad0f3a92cbcc
Alexandrine Gasc <alex.gasc@gmail.com>
2014-03-31 18:29:23 +0200

    nouv fichiers

---
+++ src/main/java/com/mycompany/app/Roman.java
@@ -0,0 +1,66 @@
+package roman;
+
+/**
+ *
+ * @author Alex
+ *
+ */
+public class Roman {
+
+	/**
+	 * fonction decode
+	 * @param nbRomain
+	 * @return un int
+	 */
+	public final int decode(final String nbRomain) {
+		int nbArabe = 0;
+		char prec = ' ';
+
+		for (int i = 0; i < nbRomain.length(); ++i) {
+			if (nbRomain.charAt(i) == 'I') {
+				nbArabe += 1;
+			}
+			else if (nbRomain.charAt(i) == 'V' && prec == 'I') {
+				nbArabe += 5 - 2;
+			}
+			else if (nbRomain.charAt(i) == 'V') {
+				nbArabe += 5;
+			}
+			else if (nbRomain.charAt(i) == 'X' && prec == 'I') {
+				nbArabe += 10 - 2;
+			}
+			else if (nbRomain.charAt(i) == 'X') {
+				nbArabe += 10;
+			}
+			else if (nbRomain.charAt(i) == 'L' && prec == 'X') {
+				nbArabe += 50 - 20;
+			}
+			else if (nbRomain.charAt(i) == 'L') {
+				nbArabe += 50;
+			}
+			else if (nbRomain.charAt(i) == 'C' && prec == 'X') {
+				nbArabe += 100 - 20;
+			}
+			else if (nbRomain.charAt(i) == 'C') {
+				nbArabe += 100;
+			}
+			else if (nbRomain.charAt(i) == 'D' && prec == 'C') {
+				nbArabe += 500 - 200;
+			}
+			else if (nbRomain.charAt(i) == 'D') {
+				nbArabe += 500;
+			}
+			else if (nbRomain.charAt(i) == 'M' && prec == 'C') {
+				nbArabe += 1000 - 200;
+			}
+			else if (nbRomain.charAt(i) == 'M') {
+				nbArabe += 1000;
+			}
+
+			prec = nbRomain.charAt(i);
+		}
+
+		return nbArabe;
+	}
+
+}
---
+++ src/main/java/com/mycompany/app/RomanTest.java
@@ -0,0 +1,130 @@
+package romanTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import roman.Roman;
+
+/**
+ *
+ * @author Alex
+ *
+ */
+public class RomanTest {
+	Roman r;
+
+	/**
+	 * fonction before
+	 */
+	@Before
+	public final void before() {
+		r = new Roman();
+	}
+
+	@Test
+	public final void testI() {
+		assertEquals(1, r.decode("I"));
+	}
+
+	@Test
+	public final void testIV() {
+		assertEquals(4, r.decode("IV"));
+	}
+
+	@Test
+	public final void testV() {
+		assertEquals(5, r.decode("V"));
+	}
+
+	@Test
+	public final void testVI() {
+		assertEquals(6, r.decode("VI"));
+	}
+
+	@Test
+	public final void testIX() {
+		assertEquals(9, r.decode("IX"));
+	}
+
+	@Test
+	public final void testX() {
+		assertEquals(10, r.decode("X"));
+	}
+
+	@Test
+	public final void testXIV() {
+		assertEquals(14, r.decode("XIV"));
+	}
+
+	@Test
+	public final void testXL() {
+		assertEquals(40, r.decode("XL"));
+	}
+
+	@Test
+	public final void testL() {
+		assertEquals(50, r.decode("L"));
+	}
+
+	@Test
+	public final void testLX() {
+		assertEquals(60, r.decode("LX"));
+	}
+
+	@Test
+	public final void testXC() {
+		assertEquals(90, r.decode("XC"));
+	}
+
+	@Test
+	public final void testC() {
+		assertEquals(100, r.decode("C"));
+	}
+
+	@Test
+	public final void testCX() {
+		assertEquals(110, r.decode("CX"));
+	}
+
+	@Test
+	public final void testCD() {
+		assertEquals(400, r.decode("CD"));
+	}
+
+	@Test
+	public final void testD() {
+		assertEquals(500, r.decode("D"));
+	}
+
+	@Test
+	public final void testDC() {
+		assertEquals(600, r.decode("DC"));
+	}
+
+	@Test
+	public final void testCM() {
+		assertEquals(900, r.decode("CM"));
+	}
+
+	@Test
+	public final void testM() {
+		assertEquals(1000, r.decode("M"));
+	}
+
+	@Test
+	public final void testMC() {
+		assertEquals(1100, r.decode("MC"));
+	}
+
+	@Test
+	public final void testMMDCCLI() {
+		assertEquals(2751, r.decode("MMDCCLI"));
+	}
+
+	@Test
+	public final void testCDXLVIII() {
+		assertEquals(448, r.decode("CDXLVIII"));
+	}
+}
